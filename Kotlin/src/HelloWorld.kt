4f46c1721d6a2e9e179ce2c8aaf7979a30cfd447
Enzo Lizama Paredes <lizama.enzo@gmail.com>
2017-05-24 00:17:23 -0500

    READLINE!

--- Kotlin/src/HelloWorld.kt
+++ Kotlin/src/HelloWorld.kt
@@ -8,11 +8,19 @@ fun main(args:Array<String>){
     var carrer:String? // THAT CAN BE NULL
     carrer= "Software Engineering"
     val pi:Double = 3.14159 // DOUBLE DATA TYPE
-    print("Hello $name you have $age years and you study $carrer")
+    println("Hello $name you have $age years and you study $carrer")
 
     //------------------------    VAR vs VAL      -------------------------------
 
     val secondName:String = "Lizama" //ITS NOT MUTABLE ( NEVER CHANGE )
+
+    // =======================    readLine      ===============================
+     var chapa = readLine()
+    println("Tu chapa es $chapa")
+    var money = readLine()!!.toDouble() // CONVIERTE A DOUBLE SE HACE
+                                        // LO MISMO PARA INT , ETC
+                                        // '!!' => PARA NON-NULL STR
+    println("Y tienes $money")
 
 
 }
