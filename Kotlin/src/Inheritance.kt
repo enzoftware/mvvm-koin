92c22b9ecda7de5b01a01259a5c1fdeb6b04902e
Enzo Lizama Paredes <lizama.enzo@gmail.com>
2017-05-31 00:54:09 -0500

    final and open inheterance visibility modifiers
    open , final ,private , protected , super

--- Kotlin/src/Inheritance.kt
+++ Kotlin/src/Inheritance.kt
@@ -3,13 +3,29 @@
  */
 
 
-class Operations(){
-   fun  sum(n1:Int, n2: Int) = n1+n2
+open class Operations(){ // normally understand a class like final
+    protected var ProcessName:String? = "Enzo"
+
+    fun  sum(n1:Int, n2: Int) = n1+n2
 
     fun div(n1:Int , n2 : Int ) = n1/n2
 }
 
+class MultiOps() : Operations(){
+
+    fun  res(n1:Int, n2: Int) = n1-n2
+
+    fun mult(n1:Int , n2 : Int ) = n1*n2
+
+    fun getName(){
+        super.ProcessName
+    }
+}
 
 fun main(args: Array<String>) {
-    
+    var ops = MultiOps()
+    var sum = ops.sum(15,45)
+    var div = ops.div(48,8)
+    println(" sum = $sum , div = $div")
+    print(" nombre " + ops.getName())
 }
