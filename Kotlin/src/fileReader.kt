682459a376986c5f9939a948848545d8fb5d7def
Enzo Lizama Paredes <lizama.enzo@gmail.com>
2017-06-10 04:32:37 -0500

    read file in kotlin

--- Kotlin/src/fileReader.kt
+++ Kotlin/src/fileReader.kt
@@ -1,7 +1,26 @@
+import java.io.FileReader
+
 /**
  * Created by enzoftware on 6/10/17.
  */
 
+fun ReadFile(){
+    try {
+        var fi = FileReader("test.txt")
+        var c:Int ?
+        do {
+            c = fi.read()
+            print(c.toChar())
+        }while ( c!= -1)
+
+
+
+    }catch (ex:Exception){
+        println(ex.message)
+    }
+}
+
 fun main(args: Array<String>) {
+    ReadFile()
     
 }
