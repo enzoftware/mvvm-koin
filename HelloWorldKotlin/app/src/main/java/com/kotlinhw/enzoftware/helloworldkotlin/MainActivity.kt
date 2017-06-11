15fcafb5e1bbd2388ef76fc408a895e8899ea2fb
Enzo Lizama Paredes <lizama.enzo@gmail.com>
2017-06-11 00:47:33 -0500

    my complete FindMyAgeAPP with kotlin

--- HelloWorldKotlin/app/src/main/java/com/kotlinhw/enzoftware/helloworldkotlin/MainActivity.kt
+++ HelloWorldKotlin/app/src/main/java/com/kotlinhw/enzoftware/helloworldkotlin/MainActivity.kt
@@ -2,7 +2,10 @@ package com.kotlinhw.enzoftware.helloworldkotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,5 +13,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        findMyAgeBtn.setOnClickListener ({
+            Toast.makeText(applicationContext,"Button is clicked", Toast.LENGTH_SHORT).show()
+            val year:String = editAge.text.toString()
+            val today:Int = Calendar.getInstance().get(Calendar.YEAR)
+            val myAge:Int = today - year.toInt()
+            showAgeTxv.text = "Your age is $myAge"
+
+        })
     }
 }
