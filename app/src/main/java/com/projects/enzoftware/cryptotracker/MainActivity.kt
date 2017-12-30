d0da1c4c945793f5b7ea2a1e40a55e247b1719d3
enzoftware <lizama.enzo@gmail.com>
2017-12-30 01:28:24 -0500

    Doing a request with AFN

--- app/src/main/java/com/projects/enzoftware/cryptotracker/MainActivity.kt
+++ app/src/main/java/com/projects/enzoftware/cryptotracker/MainActivity.kt
@@ -2,11 +2,35 @@ package com.projects.enzoftware.cryptotracker
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.androidnetworking.AndroidNetworking
+import com.androidnetworking.common.ANRequest
+import com.androidnetworking.common.Priority
+import com.androidnetworking.error.ANError
+import com.androidnetworking.interfaces.JSONArrayRequestListener
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONArray
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AndroidNetworking.initialize(applicationContext)
+        AndroidNetworking .get("https://api.coinmarketcap.com/v1/ticker/")
+                .setPriority(Priority.LOW)
+                .build()
+                .getAsJSONArray(object : JSONArrayRequestListener{
+                    override fun onResponse(response: JSONArray?) {
+                        hw.text = response.toString()
+                    }
+
+                    override fun onError(anError: ANError?) {
+                        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                    }
+
+                })
     }
 }
+
+
